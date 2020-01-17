719beb654464a1ad2b977c4215b7ac850ddd63bd
Jeffrey <jeffreymartespolanco@hotmail.com>
2020-01-17 13:39:01 -0800

    MVP DONE

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import axios from "axios";
+
+import NightButton from "./Components/button";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => this.setState({ players: res.data }));
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <NightButton />
+        {this.state.players.map(people => (
+          <div>
+            <h1>{people.name}</h1>
+            <h2>{people.country}</h2>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as rt1 from "@testing-library/react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+afterEach(rt1.cleanup);
+
+test("displays toggle function", () => {
+  const { getByTestId } = rt1.render(<App />);
+  getByTestId("toggle");
 });
